0b45b2c250dd77eef673228e5b05775a9aaa02b1
Bilel Jabrane <bileljabrane6@gmail.com>
2025-01-31 08:34:01 +0100

    First commit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,16 @@
 import express from 'express'
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
 import connectDB from './config/connectDB.js';
 import authRoutes from './routes/authRoutes.js';
-import taskRoutes from './routes/taskRoute.js';
 
 const port = process.env.PORT || 5000
 const app = express()
 dotenv.config()
-app.use(
-    cors({
-        origin: 'http://localhost:4200',
-        credentials: true,
-    }))
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth', authRoutes)
-app.use('/api/tasks', taskRoutes)
 
 connectDB()
 app.listen(port, () => {
--- frontend/src/app/States/taskState.ts
+++
@@ -1,7 +0,0 @@
-import { Task, TaskCategory } from "../models/task.model";
-export const taskStatus: TaskCategory[] = [
-    { status: 'todo', tasks: [] },
-    { status: 'inProgress', tasks: [] },
-    { status: 'completed', tasks: [] },
-  ];
-
